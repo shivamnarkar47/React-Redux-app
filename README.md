4fe59d6f67c6002fa58981693b0a61318ba2efa3
shivamnarkar47 <shivamnarkar47@gmail.com>
2021-08-09 21:37:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-owbvud
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-owbvud)
---
+++ react-redux/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to my first React - Redux project</h1>
+    </div>
+  );
+}
+
+export default App;
